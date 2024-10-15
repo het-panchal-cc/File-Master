a71867bd000af7591c2acfbfcbd25d9229cf24a0
Arkit-developer <patelarkit243@gmail.com>
2024-10-15 14:31:50 +0530

    login page design

--- app/routes/_index/route.jsx
+++ app/routes/_index/route.jsx
@@ -1,7 +1,9 @@
 import { json, redirect } from "@remix-run/node";
 import { Form, useLoaderData } from "@remix-run/react";
 import { login } from "../../shopify.server";
-import styles from "./styles.module.css";
+import indexStyles from "./styles.module.css";
+
+export const links = () => [{ rel: "stylesheet", href: indexStyles }];
 
 export const loader = async ({ request }) => {
   const url = new URL(request.url);
@@ -17,38 +19,30 @@ export default function App() {
   const { showForm } = useLoaderData();
 
   return (
-    <div className={styles.index}>
-      <div className={styles.content}>
-        <h1 className={styles.heading}>A short heading about [your app]</h1>
-        <p className={styles.text}>
-          A tagline about [your app] that describes your value proposition.
+    <div datatype="index">
+      <div datatype="content">
+        <img
+          src="https://cdn.shopify.com/s/files/1/0578/4656/2880/files/filemaster_logo.jpg?v=1728977740"
+          alt="login_logo"
+          style={{ margin: "0 auto" }}
+          height="100"
+          width="100"
+        />
+        <h1>Welcome to FileMaster: Files Exporter App</h1>
+        <p>
+          Easily download all your media files (images, videos, etc.) from the
+          FileMaster : Files Exporter app.
         </p>
         {showForm && (
-          <Form className={styles.form} method="post" action="/auth/login">
-            <label className={styles.label}>
-              <span>Shop domain</span>
-              <input className={styles.input} type="text" name="shop" />
+          <Form method="post" class="form" action="/auth/login">
+            <label>
+              <span datatype="labelSpan">Shop domain:</span>
+              <input type="text" name="shop" />
               <span>e.g: my-shop-domain.myshopify.com</span>
             </label>
-            <button className={styles.button} type="submit">
-              Log in
-            </button>
+            <button type="submit">Log in</button>
           </Form>
         )}
-        <ul className={styles.list}>
-          <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
-          </li>
-          <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
-          </li>
-          <li>
-            <strong>Product feature</strong>. Some detail about your feature and
-            its benefit to your customer.
-          </li>
-        </ul>
       </div>
     </div>
   );
